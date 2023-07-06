89534a88dc80c0b0858fc0e483c8838098baeadf
MUNYESHURImanzi <munyeshurimanzi@gmail.com>
2023-07-06 02:43:32 -0700

    type script

---
+++ Routes/productRoute.ts
@@ -0,0 +1,12 @@
+import  {createProduct, getAllProduct } from "../../../Controllers/productController";
+import express from "express"
+
+
+const router=express.Router()
+
+
+router.post("/create",createProduct)
+router.get("/allProducts",getAllProduct)
+
+
+export default router
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import productRoute from '../typescript-project/Controllers/Routes/productRoute';
+import cartRoute from '../typescript-project/Controllers/Routes/cartRoute'
+
+dotenv.config();
+
+//connect to mongo db
+
+const MongoDB_connection_string ="mongodb+srv://eddynzobarinda910:makavela123@cluster0.sbzptlt.mongodb.net/?retryWrites=true&w=majority";
+async function connectToMongoDB(connectionstring: string) {
+  await mongoose.connect(connectionstring);
+  console.log("connected to the database successfully!");
+}
+try {
+  connectToMongoDB(MongoDB_connection_string)
+}catch (e) {
+  console.log("error occured while connecting to the database: ",e);
+}
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors());
+app.use(cookieParser());
+const port = 5500;
+
+//creating product
+
+app.use("/product",productRoute)
+//create cart
+app.use("/cart",cartRoute)
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('MWIRIWE ARIKO');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port 5500`);
+});
